c9a6f6dc2c573d65d89ab6aaf71b6ac8fc46aa1a
Althaf CH <mohammedalthaf22022@gmail.com>
2026-06-11 22:20:10 +0530

    changed port from locally for deployment

--- back-end/server.js
+++ back-end/server.js
@@ -290,8 +290,10 @@ app.put("/edit-task/:id", (req, res) => {
     );
 
 });
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
 
-    console.log("Server running on port 5000");
+app.listen(PORT, () => {
+
+    console.log(`Server running on port ${PORT}`);
 
 });
